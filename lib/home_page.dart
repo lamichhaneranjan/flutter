2e3b59d46bb871e1a284743b14e82d9f1d54b3b0
Ranjan <lamichhaneranjan1@gmail.com>
2022-11-22 23:08:20 +0545

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Catalog App")),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to 1 days of flutter by"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/main.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int day = 10;
+    String name = "ranjan";
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Container(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
